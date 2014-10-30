f9354ad793059f5a1524b6e74a4622297e0c3b61
rociomendoza <rociomendoza234@gamil.com>
2014-10-30 09:24:25 -0600

    avance del dia 291014

--- drupal-7.32/sites/all/themes/mundo/templates/page.tpl.php
+++ drupal-7.32/sites/all/themes/mundo/templates/page.tpl.php
@@ -73,7 +73,6 @@
  * @ingroup themeable
  */
 ?>
-<?php print render($page['123']); ?>
 
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
 <div id="EdV">
@@ -91,92 +90,50 @@
         <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
       </a>
       <?php endif; ?>
-  <div id="hora">Hora:                fecha:</div>
+  <div id="hora">
+<time pubdate datetime="2014-10-29T17:51:55+01:0000Z">29 de octubre de 2014, actualizado a las <strong>17:51</strong> horas</time>
+ </div>
 </div>
 
 </header>
+
 <div class="container">
-    <div class="navbar-header">
-      <?php if (!empty($site_name)): ?>
-      <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
-
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
-
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+   
+    <?php if (!empty($primary_nav) ||  !empty($page['navigation'])): ?>
       <div class="navbar-collapse collapse">
         <nav role="navigation">
           <?php if (!empty($primary_nav)): ?>
             <?php print render($primary_nav); ?>
           <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
+          
         </nav>
       </div>
     <?php endif; ?>
 </div>
-<div id="es_noticias"></div>
-<div class="main-container container">
-
-  <header role="banner" id="page-header">
-    <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
-    <?php endif; ?>
-
-    <?php print render($page['header']); ?>
-  </header> <!-- /#page-header -->
-
-  <div class="row">
-
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
-    <?php endif; ?>
-
-    <section<?php print $content_column_class; ?>>
-      <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
-      <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-    </section>
-
-    <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_second']); ?>
-      </aside>  <!-- /#sidebar-second -->
-    <?php endif; ?>
-
-  </div>
+<div class="es_noticias">
+    <ul>
+   Noticias: 
+    <li  id="uu" >Noticias 1</li>
+    <li  id="uu" >Noticias 2</li>
+    <li  id="uu" >Noticias 3</li>
+    <li  id="uu" >Noticias 4</li>
+    <li  id="uu" >Noticias 5</li>
+    <li  id="uu" >Noticias 6</li>
+    <li  id="uu" >Noticias 7</li>
+  </ul>
 </div>
+
+<div class="main-container">
+ <?php print render($page['columna1']); ?>
+ <?php print render($page['columna2_h']); ?>
+    
+
+
+
+
+
+</div>
+
 <footer class="footer container">
   <?php print render($page['footer']); ?>
 </footer>
